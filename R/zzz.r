02c6eb555a0be920d7d40681e56635bff43e4e16
Gibran Hemani <explodecomputer@gmail.com>
2017-12-22 18:34:12 +0000

    updated

---
+++ R/zzz.r
@@ -0,0 +1,20 @@
+.onLoad <- function(libname, pkgname) {
+
+	a <- suppressWarnings(try(readLines("https://raw.githubusercontent.com/MRCIEU/MRInstruments/master/DESCRIPTION"), silent=TRUE))
+
+	if(!class(a) == 'try-error')
+	{
+		latest <- gsub("Version: ", "", a[grep("Version", a)])
+		current = utils::packageDescription('MRInstruments')
+
+		test <- utils::compareVersion(latest, current$Version)
+		if(test == 1)
+		{
+			packageStartupMessage("\nWarning:\nYou are running an old version of the MRInstruments package.\n",
+				"This version:   ", current$Version, "\n",
+				"Latest version: ", latest, "\n",
+				"Please consider updating using devtools::install_github('MRCIEU/MRInstruments')")
+		}
+	}
+	invisible()
+}
