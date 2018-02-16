d531a8baa998345b443aefa96dd3b869aa6d1d25
denisbrd <33153504+denisbrd@users.noreply.github.com>
2018-02-16 17:04:05 +0000

    Add files via upload
    Added corresponding R file for monocyte data

---
+++ R/blueprint_monocyte_tophits.R
@@ -0,0 +1,16 @@
+#' Data frame of eQTLs (monocyte cells) from the BLUEPRINT study
+#' Data was downloaded from \url{ftp://ftp.ebi.ac.uk/pub/databases/blueprint/blueprint_Epivar/qtl_as/QTL_RESULTS/mono_gene_nor_combat_peer_10_all_summary.txt.gz}.  
+#'Top hit SNPs filtered for (all associations with P<5e-8).
+#' @format A data frame with 452537 rows and 8 columns
+#'  \describe{
+#'  \item{Phenotype}{Gene name}
+#'  \item{SNP}{SNP rs ID}
+#'  \item{beta}{Effect size}
+#'  \item{se}{Standard error}
+#'  \item{eaf}{Effect allele frequency}
+#'  \item{effect_allele}{Effect allele}
+#'  \item{other_allele}{Non effect allele}
+#'  \item{pval}{p-value}
+#'  }
+#'
+#' @source \url{https://scmv-ieugit.epi.bris.ac.uk/gh13047/blueprint_monocyte_tophits}
