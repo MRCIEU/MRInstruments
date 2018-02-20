e3540761da239219213a3ed8ff633a8a8f20b7e8
denisbrd <epdab@bristol.ac.uk>
2018-02-20 13:18:13 +0000

    updated documentation for blueprint

--- R/blueprint_monocyte_tophits.R
+++ R/blueprint_monocyte_tophits.R
@@ -1,6 +1,8 @@
 #' Data frame of eQTLs (monocyte cells) from the BLUEPRINT study
+#'
 #' Data was downloaded from \url{ftp://ftp.ebi.ac.uk/pub/databases/blueprint/blueprint_Epivar/qtl_as/QTL_RESULTS/mono_gene_nor_combat_peer_10_all_summary.txt.gz}.  
-#'Top hit SNPs filtered for (all associations with P<5e-8).
+#' Top hit SNPs filtered for (all associations with P<5e-8).
+#'
 #' @format A data frame with 452537 rows and 8 columns
 #'  \describe{
 #'  \item{Phenotype}{Gene name}
@@ -14,3 +16,4 @@
 #'  }
 #'
 #' @source \url{https://scmv-ieugit.epi.bris.ac.uk/gh13047/blueprint_monocyte_tophits}
+"blueprint_monocyte_eqtl"
